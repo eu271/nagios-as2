34c1472de282eac58ccf4fb925b820e3cbe2b0f8
Eugenio Ochoa Lopez <eugenio_oso@hotmail.com>
2015-03-28 13:17:22 +0100

    Mas bugfix, cosas concretas de apache2 y ubuntu14_04.

--- tools/install-nagios-4.sh
+++ tools/install-nagios-4.sh
@@ -34,14 +34,17 @@ FILE_NAGIOS_PLUGINS=$(echo ${URL_NAGIOS_PLUGINS}| cut -d/ -f5)
 DIR_NAGIOS_CORE=$(echo ${FILE_NAGIOS_CORE}| cut -d. -f1,2,3)
 DIR_NAGIOS_PLUGINS=$(echo ${FILE_NAGIOS_PLUGINS}| cut -d. -f1,2)
 
+DEFAULTPASS="defaultPass"
+
 #Instala todas las librerias y extras para el funcionamiento de nagios como servidor
 #Ademas tambien añade todo lo necesario para la configuracion y funcionamiento CGI
-apt-get install -y wget build-essential apache2 libgd2-xpm-dev libapache2-mod-php5
+apt-get install -y wget build-essential apache2 apache2-utils libgd2-xpm-dev libapache2-mod-php5
 
 #Añade los usuarios pertinentes para el funcionamiento de nagios ademas de los grupos
 #para compartir permisos.
 echo "Añadiendo usuario para nagios:" 
-useradd nagios
+useradd -m -S /bin/bash nagios
+echo -e "${DEFAULTPASS}\n${DEFAULTPASS}\n" | passwd nagios
 groupadd nagcmd
 usermod -a -G nagcmd nagios
 usermod -a -G nagcmd www-data
@@ -70,8 +73,12 @@ make install-webconf
 cp -R contrib/eventhandlers/ /usr/local/nagios/libexec/
 chown -R nagios:nagios /usr/local/nagios/libexec/eventhandlers
 
+#SOLO EN UBUNTU 14.04 LTS APACHE2, los ficheros de apache van a lugares distintos que los estandar.
+/usr/bin/install -c -m 644 sample-config/httpd.conf /etc/apache2/conf-available/nagios.conf
+ln -s /etc/apache2/conf-available/nagios.conf /etc/apache2/conf-enabled/nagios.conf
+
 echo "Añadiendo usuario para la web:"
-htpasswd –c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+echo -e "${DEFAULTPASS}\n${DEFAULTPASS}\n" | htpasswd –c /usr/local/nagios/etc/htpasswd.users nagiosadmin
 
 
 #Instalacion de los plugins en nagios
@@ -86,5 +93,7 @@ ln -s /etc/init.d/nagios /etc/rcS.d/S99nagios
 
 
 /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+#Habilitar cgi en apache2
+a2enmod cgi
 /etc/init.d/apache2 reload && /etc/init.d/nagios start
 
